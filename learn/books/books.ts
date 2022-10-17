249c68f9a358956cd40003d07b1d8f06548afd83
Luis Gerardo Manrique Cardona <lmanrique@truora.com>
2022-10-17 14:54:46 -0500

    Add world and learn

---
+++ learn/books/books.ts
@@ -0,0 +1,8 @@
+export const Books = [
+    'El mundo como voluntad y representacion volumen 1',
+    'El mundo como voluntad y representacion volumen 2',
+    'Critica de la razon pura',
+    'Investigaciones filosoficas',
+    'The ecological approach to visual perception',
+    'The '
+]
---
+++ world/continents/europe/countries/germany/states.ts
@@ -0,0 +1,16 @@
+export enum States {
+    Baviera,
+    Berlin,
+    FreeSajonia,
+    Turingia,
+    OccidentalPomerania,
+    SuthSajonia,
+    Bremen,
+    Brandeburgo,
+    BadenWurtemberg,
+    NorthRenania,
+    RenaniaPalatinado,
+    Sarre,
+    SchleswigHolstein,
+    
+}
