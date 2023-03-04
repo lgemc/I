7129d7acb1221eb60c974baec568598eba0ac2d9
Luis Gerardo Manrique Cardona <lmanrique@truora.com>
2023-03-04 15:14:17 -0500

    Allow http trafic

--- compute/infra/compute/security-groups/main-group.tf
+++ compute/infra/compute/security-groups/main-group.tf
@@ -12,6 +12,15 @@ resource "aws_security_group" "main_security_group" {
   }
 
   ingress {
+    description      = "Allow http"
+    from_port        = 0
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
     description      = "Allow kubectl"
     from_port        = 0
     to_port          = 6443
