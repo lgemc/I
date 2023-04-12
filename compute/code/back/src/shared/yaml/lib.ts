613460e0c6b2a6a0772eb718fb38fc47f60f6516
Luis Gerardo Manrique Cardona <luis.manrique.car@gmail.com>
2023-04-11 23:41:53 -0500

    Add libraries to manage templates yaml

---
+++ compute/code/back/src/shared/yaml/lib.ts
@@ -0,0 +1,12 @@
+import { Result, err, ok } from "neverthrow";
+import yaml from "js-yaml";
+
+function parseString(input: { data: string }): Result<string, Error> {
+  try {
+    const data = yaml.load(input.data);
+
+    return ok(data);
+  } catch (e) {
+    return err(e);
+  }
+}
